152d486b863f6bd31b130fdce56868634f3801c0
Andrew White <andyw@pixeltrix.co.uk>
2015-05-18 16:40:17 +0100

    Add basic product feature
    Create products feature using scaffold generator and add feature specs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.find_or_create_by!(name: "Nike T-Shirt") do |p|
+  p.description = "Men's T-Shirt in red"
+  p.price       = "15.00"
+end
