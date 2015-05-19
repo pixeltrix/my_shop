ce99ed87786fc54b058adb8ef5b888ddd9e8dc7e
Andrew White <andyw@pixeltrix.co.uk>
2015-05-19 12:15:14 +0100

    Add dismissible flash messages
    Use the Bootstrap alerts component to make our flash messages dismissible.

---
+++ spec/features/alerts_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature "Alerts" do
+  scenario "notice flash messages are dissmissable", js: true do
+    visit '/products/new'
+
+    fill_in 'Name', with: "Nike T-Shirt"
+    fill_in 'Description', with: "Men's T-Shirt in red"
+    fill_in 'Price', with: "15.00"
+
+    click_button 'Create Product'
+    expect(page).to have_text("Product was successfully created.")
+
+    within '.alert-success' do
+      click_button 'Close'
+    end
+
+    expect(page).not_to have_text("Product was successfully created.")
+  end
+
+  scenario "alert flash messages are dissmissable", js: true do
+    visit '/products/new'
+
+    fill_in 'Name', with: "Nike T-Shirt"
+    fill_in 'Description', with: "Men's T-Shirt in red"
+    fill_in 'Price', with: ""
+
+    click_button 'Create Product'
+    expect(page).to have_text("Failed to create product - please check for errors.")
+
+    within '.alert-danger' do
+      click_button 'Close'
+    end
+
+    expect(page).not_to have_text("Failed to create product - please check for errors.")
+  end
+end
