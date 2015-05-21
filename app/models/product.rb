bde7782cdac25e78d8364c418495f430702a67aa
Andrew White <andyw@pixeltrix.co.uk>
2015-05-21 10:38:32 +0100

    Add colour attribute to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,13 @@ class Product < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 2000 }
   validates :price, presence: true, numericality: { greater_than: 0, less_than: 10000 }
+  validates :colour, length: { maximum: 30 }
 
+  alias_attribute :color, :colour
+
+  scope :colour, ->(colour) { where(colour: colour) }
+
+  def self.default_scope
+    order(arel_table[:created_at].desc)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519142139) do
+ActiveRecord::Schema.define(version: 20150520140425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150519142139) do
     t.decimal  "price",                   precision: 6, scale: 2, null: false
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "colour",      limit: 30
   end
 
   add_index "products", ["name"], name: "index_products_on_name", unique: true, using: :btree
