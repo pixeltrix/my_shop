3fba9d6a8407cd4a5b3d6ee0c52b52a9a2b0e1bf
Andrew White <andyw@pixeltrix.co.uk>
2015-05-21 10:37:33 +0100

    Add Category model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  has_and_belongs_to_many :categories
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 2000 }
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,22 @@ ActiveRecord::Schema.define(version: 20150519142139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",        limit: 100, null: false
+    t.string   "slug",        limit: 100, null: false
+    t.text     "description"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "product_id",  null: false
+    t.integer "category_id", null: false
+  end
+
+  add_index "categories_products", ["category_id"], name: "index_categories_products_on_category_id", using: :btree
+  add_index "categories_products", ["product_id"], name: "index_categories_products_on_product_id", using: :btree
+
   create_table "products", force: :cascade do |t|
     t.string   "name",        limit: 100,                         null: false
     t.text     "description",                                     null: false
